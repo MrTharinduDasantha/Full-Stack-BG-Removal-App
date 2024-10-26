f31396ef919effdbfe6aafd9c3ae76a3feb122e2
MrTharinduDasantha <tharindudasantha2001e@gmail.com>
2024-10-26 12:22:06 +0530

    Update Server.js File

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import connectDB from "./config/mongodb.js"
-import userRouter from "./routes/userRoutes.js"
 
 // App configuration
 const PORT = process.env.PORT || 5000
@@ -17,6 +16,5 @@ app.use(cors())
 
 // Api Routes
 app.get("/", (req, res) => res.send("Welcome to Full Stack BG Removal Website"))
-app.use("/api/user", userRouter)
 
 app.listen(PORT, () => console.log("Server running on port " + PORT))
